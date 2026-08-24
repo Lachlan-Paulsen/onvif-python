343883e6176635bef356923061394a34862259b6
Kaburagi <kaburagisec@proton.me>
2026-08-24 09:02:33 +0700

    feat: Add repo variable for library and comply formatting

--- onvif/__init__.py
+++ onvif/__init__.py
@@ -1,6 +1,10 @@
-# onvif/__init__.py
+"""onvif-python: A Python library for interacting with ONVIF-compliant devices."""
 
-__version__ = "0.2.10"
+from typing import Final
+
+
+__version__: Final[str] = "0.2.10"
+__repository__: Final[str] = "https://github.com/nirsimetri/onvif-python"
 
 from .cli import main as ONVIFCLI
 from .client import ONVIFClient
@@ -25,4 +29,5 @@ __all__ = [
     "ONVIFDiscovery",
     "ONVIFParser",
     "__version__",
+    "__repository__",
 ]
