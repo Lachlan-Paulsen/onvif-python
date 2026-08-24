e357c1da9366ce36f57fb6131cf3fe239a410889
Kaburagi <kaburagisec@proton.me>
2026-08-24 11:01:08 +0700

    style(cli): Implement pylint conventions on the CLI.

--- onvif/cli/__main__.py
+++ onvif/cli/__main__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-# onvif/cli/__main__.py
-
-"""
-ONVIF CLI entry point for python -m onvif.cli
-"""
+"""ONVIF CLI entry point for python -m onvif.cli"""
 
 from .main import main
 
